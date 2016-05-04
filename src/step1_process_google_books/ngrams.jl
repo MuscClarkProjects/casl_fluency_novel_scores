a58502dc425cdaa19402223eca27a8c3d6b2d523
lbuser <lbuser@noreply.github.com>
2016-05-04 13:55:47 -0400

    parallel, but not writing to log file

--- src/step1_process_google_books/ngrams.jl
+++ src/step1_process_google_books/ngrams.jl
@@ -1,7 +1,6 @@
 using Logging
 
 validword_re = r"^[a-zA-Z|-|']+$"
-validbigram_re = r"^[a-zA-Z|-|']+ [a-zA-Z|-|']+$"
 
 function isValidGram(text::AbstractString, gram::Int64)
   if !isvalid(ASCIIString, text)
@@ -22,14 +21,14 @@ end
 isValidGram{T <: AbstractString}(text::SubString{T}, gram::Int64) = isValidGram(convert(T, text), gram)
 
 
-isValidRow(leftright, count, gram::Int64) = isValidGram(leftright, gram) && ismatch(r"^[0-9]+$", count)
+isValidRow(text, count, gram::Int64) = isValidGram(text, gram) && ismatch(r"^[0-9]+$", count)
 
 
 isValidRow{T <: AbstractString}(cols::AbstractVector{T}, gram::Int64) =  (length(cols) > 2) &&
   isValidRow(cols[1], cols[3], gram)
 
 
-function getLeftrightCount{T <: AbstractString}(cols::AbstractVector{T})
+function getGramTextAndCount{T <: AbstractString}(cols::AbstractVector{T})
   convert(ASCIIString, lowercase(cols[1])), parse(Int64, cols[3])
 end
 
@@ -40,8 +39,8 @@ squishCounts(lines, gram::Int64) = reduce(Dict{ASCIIString, Int64}(), lines) do 
   end
   cols = split(line, '\t')
   if isValidRow(cols, gram)
-    leftright::ASCIIString, count::Int64 = getLeftrightCount(cols)
-    acc[leftright] = get(acc, leftright, 0) + count
+    text::ASCIIString, count::Int64 = getGramTextAndCount(cols)
+    acc[text] = get(acc, text, 0) + count
   end
   acc
 end
@@ -49,7 +48,7 @@ end
 
 squishCounts(io::IOStream, gram::Int64) = squishCounts(eachline(io), gram)
 
-squishCountsGen(gram::Int64) = lines -> squishCounts(lines, gram)
+squishCountsGen(gram::Int64) = io -> squishCounts(io, gram)
 squishCounts(f_name::AbstractString, gram::Int64) = open(squishCountsGen(gram), f_name, "r")
 
 
@@ -58,7 +57,61 @@ function genUrl(left::Char, right::Char, gram::Int64)
 end
 
 
-function main(gram::Int64, dest_dir::AbstractString, start_from::Int64=1)
+type TwoChar
+  left::Char
+  right::Char
+
+  TwoChar(left, right) = begin
+    for c::Char in (left, right)
+      islower(c) || error("$c must be lowercase alpha-numeric")
+    end
+    new(left, right)
+  end
+end
+
+
+genUrl(tc::TwoChar, gram::Int64) = genUrl(tc.left, tc.right, gram)
+
+
+Base.convert(::Type{Int64}, tc::TwoChar) = begin
+  val(c::Char) = Int(c) - Int('a')
+  26*val(tc.left) + val(tc.right)
+end
+
+
+function +(tc::TwoChar, bias::Int64)
+  if sign(bias) < 0
+    return tc - abs(bias)
+  end
+  right::Char = tc.right + bias%26
+  left::Char =  tc.left + floor(Int64, bias/26)
+  is_rollover::Bool = right > 'z'
+  if is_rollover
+    left += 1
+    right -= 26
+  end
+  TwoChar(left, right)
+end
+
+
+function -(tc::TwoChar, bias::Int64)
+  if sign(bias) < 0
+    return tc + abs(bias)
+  end
+  right::Char = tc.right - bias%26
+  left::Char =  tc.left - floor(Int64, abs(bias)/26)
+  is_rollover::Bool = right < 'a'
+  if is_rollover
+    left -= 1
+    right += 26
+  end
+  TwoChar(left, right)
+end
+
+
+function main(gram::Int64, dest_dir::AbstractString; 
+  start_from::TwoChar=TwoChar('a', 'a'), 
+  run_until::TwoChar = TwoChar('z', 'z'))
 
   log_f::ASCIIString = "$gram.log"
   isfile(log_f) && rm(log_f)
@@ -66,27 +119,21 @@ function main(gram::Int64, dest_dir::AbstractString, start_from::Int64=1)
   Logging.configure(filename="$(gram).log")
   Logging.configure(level=INFO)
 
-  a_through_z::Vector{Char} = map(Char, 97:(97+25))
+  urls::Vector{ASCIIString} = begin
+    num_extra_downloads = Int(run_until) - Int(start_from)
+    map(ix::Int64 -> genUrl(start_from + ix, gram), 0:num_extra_downloads)
+  end
 
-  current_ix::Int64 = 0
-  for left::Char in a_through_z
-    for right::Char in a_through_z
-      current_ix += 1
-      if current_ix < start_from
-        continue
-      end
-      url = genUrl(left, right, gram)
-      info("processing $(current_ix): $url")
+  pmap(urls) do url::ASCIIString
+    info("processing $url")
 
-      f::AbstractString = downloadLargeFile(url, dest_dir)
-      println("calculating counts for $f")
-      counts::Dict{ASCIIString, Int64} = squishCounts(f, gram)
-      f_counts = replace(f, ".tsv", "_counts.tsv")
-      writedlm(f_counts, counts)
-      println("$(f_counts) calculated")
-      rm(f)
-
-    end
+    f::AbstractString = downloadLargeFile(url, dest_dir)
+    println("calculating counts for $f")
+    counts::Dict{ASCIIString, Int64} = squishCounts(f, gram)
+    f_counts = replace(f, ".tsv", "_counts.tsv")
+    writedlm(f_counts, counts)
+    println("$(f_counts) calculated")
+    rm(f)
   end
 
 end
