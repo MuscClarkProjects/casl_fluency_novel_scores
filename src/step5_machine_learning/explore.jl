34ea2396a7f1376dbeeed136010ee345ae1b4267
lbuser <lbuser@noreply.github.com>
2016-07-25 18:24:39 -0400

    use built-in complete_cases for handling NAs

--- src/step5_machine_learning/explore.jl
+++ src/step5_machine_learning/explore.jl
@@ -10,22 +10,16 @@ function readTask(task, baseline_only=true)
 end
 
 
-allNas(task, include_y=true) = @> task readTask allNas(include_y)
-
-function allNas(task::DataFrame, include_y=true)
-  data_cols = include_y ? names(task) : @> task names setdiff([summary_cols; :id])
-
-  na_rows = reduce(Vector{Int64}(), data_cols) do v, c::Symbol
-    [v; task[c] |> isna |> find]
-  end
-
-  @>> na_rows setdiff(1:size(task, 1)) tuple(na_rows)
-end
-
-
 pureData(task, include_y=true) = @> task readTask pureData(include_y)
 
 function pureData(task::DataFrame, include_y=true)
-  pure_rows = @> task allNas(include_y) getindex(2)
+  data_cols = if include_y
+    names(task)
+  else
+    @> task names setdiff([summary_cols; :id])
+  end
+
+  pure_rows = @> task[:, data_cols] complete_cases
+
   task[pure_rows, :]
 end
